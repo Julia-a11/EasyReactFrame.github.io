dbc6b0cd4833af62464016d2d0354c54d6df799a
irlix <yu.shipunowa@yandex.ru>
2022-02-14 15:44:46 +0300

    add media + regex

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 import MyInput from "./UI/Input/MyInput";
 import classes from "./styles/app.scss"
 import MyButton from "./UI/Button/MyButton";
+import { Field } from 'react-final-form'
 
 
 const Form = ({create}) => {
 
     const [note, setNote] = useState({numberNote: '', textNote: ''})
+    const [regexForNumber] = useState(["e", "E", "+", "-", ".", ","]);
 
     const addNewNote = () => {
         const newNote = {
@@ -23,12 +25,20 @@ const Form = ({create}) => {
         }
     }
 
+    const getValidator = isRequired =>
+        isRequired ? value => (value ? undefined : "Required") : () => {};
+    const Error = ({ name }) => (
+        <Field name={name} subscription={{ error: true }}>
+            {({ meta: { error } }) => (error ? <span>{error}</span> : null)}
+        </Field>
+    );
 
     return (
         <div className={classes.form}>
             <form>
                 <label>Номер записи</label>
-                <MyInput  type="number" value={note.numberNote}
+                <MyInput type="number" value={note.numberNote}
+                         onKeyDown={e => regexForNumber.includes(e.key) && e.preventDefault()}
                          onChange={e => setNote({...note, numberNote: e.target.value})}/>
                 <label>Название записи</label>
                 <MyInput type="text" value={note.textNote} onKeyDown={(e) => clickEnter(e) }
